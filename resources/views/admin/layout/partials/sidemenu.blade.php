e674b134e251acce2c47a45e610b1b5c6cea6b59
Razi Kallayi <razikallayi@gmail.com>
2017-08-21 17:52:23 +0530

    added admin panel

---
+++ resources/views/admin/layout/partials/sidemenu.blade.php
@@ -0,0 +1,24 @@
+<li id="mnu-dashboard" class="header">
+  <a href="{{url('admin/dashboard')}}">
+   <span>DASHBOARD</span>
+ </a>
+</li>
+
+
+<li id="mnu-bussiness" >
+  <a href="javascript:void(0);" class="menu-toggle">
+   <span>My Bussiness</span>
+ </a>
+ <ul class="ml-menu">
+  <li class="add">
+    <a href="{{url('admin/bussiness/add')}}">
+      Add Bussiness
+    </a>
+  </li>
+  <li class="manage">
+    <a href="{{url('admin/bussiness')}}">
+      Manage Bussiness
+    </a>
+  </li>
+</ul>
+</li>
